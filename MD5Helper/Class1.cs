733c260a1727a578b4683bfc5b0cd367495d8f9d
baby0112u <baby0112u@163.com>
2017-12-23 11:15:15 +0800

    '初始提交'

---
+++ MD5Helper/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD5Helper
+{
+    public class Class1
+    {
+        public string CalcMD5()
+        {
+            return "";
+        }
+    }
+}
